1bdec5f4bb1daf519b2340a39a4b3dd9e72089cd
Luc Kadletz <luc.kadletz@digipen.edu>
2016-10-24 16:54:33 -0700

    Added Physics Demo Project

---
+++ Physics Simulation Demo/Assets/PhysicsBody.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsBody : MonoBehaviour {
+
+
+    public float mass;
+    public Vector3 acceleration;
+    public Vector3 velocity;
+    public Vector3 position
+    {
+        get { return transform.position;    }
+        set { transform.position = value;   }
+    }
+
+	// Use this for initialization
+	void Start () {
+        Debug.Assert(GetComponent<Rigidbody>() == null,
+            "Rigidbody and PhysicsBody are incompatible!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    // See PhysicsSolver.cs
+	}
+}
---
+++ Physics Simulation Demo/Assets/PhysicsSolver.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsSolver : MonoBehaviour {
+
+    public float gravity_constant;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PhysicsBody[] list = FindObjectsOfType<PhysicsBody>();
+        foreach (PhysicsBody b in list)
+        {
+            if (!b.isActiveAndEnabled) continue;
+
+            // Apply Gravity    
+            b.acceleration = ComputeGravityForce(list, b);
+            b.position += b.velocity * Time.deltaTime;
+            b.velocity += b.acceleration * Time.deltaTime;
+        }
+	}
+
+    private Vector3 ComputeGravityForce(PhysicsBody[] list, PhysicsBody body)
+    {
+        // F_g = G * m_1 * m_2 / r^2
+        Vector3 Fsum = Vector3.zero;
+        foreach(PhysicsBody b in list)
+        {
+            if (b == body) continue;
+            Vector3 direction = b.position - body.position;
+            float sqdist = direction.sqrMagnitude;
+            direction.Normalize();
+            Fsum += (gravity_constant * b.mass * body.mass / sqdist) * direction;
+        }
+        return Fsum / body.mass;
+    }
+}
