ae5c0dbdfdc30f02852dbbbd01666c9dc6f89d20
Luc Kadletz <luckadletz@gmail.com>
2019-12-01 23:14:16 -0800

    Cleaned up flyer prototype
    There isn't really much here... :airplane:

---
+++ FlyerPrototype/Assets/Flyer/Scripts/LineTrail.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LineTrail : MonoBehaviour
+{
+    // How many points we want on this line
+    public float maxPoints;
+    // Ref to LineRenderer for performance/convenience
+    private LineRenderer line;
+
+	void Start ()
+    {
+        line = GetComponent<LineRenderer>();
+        // This effect doesn't make much sense in local space
+        line.useWorldSpace = true;
+        // Get rid of previous line
+        line.positionCount = 0;
+	}
+	
+	void Update ()
+    {
+        AddPoint(transform.position);
+	}
+
+    void AddPoint(Vector3 point)
+    {
+        if(line.positionCount < maxPoints)
+        {
+            // If we're not at max points, add one more place
+            line.positionCount = line.positionCount + 1;
+        }
+        // Shift all points down one in the array, dropping the last point
+        for(int i = line.positionCount - 1; i > 0; --i)
+        {
+            Vector3 temp = line.GetPosition(i-1);
+            line.SetPosition(i, temp);
+        }
+        // Set the last point in the line to the new position
+        line.SetPosition(0, point);
+    }
+}
